98c0fe7d56194c55ef66444ef85c5d4787de1f3a
khalil Hussaini <107269671+khalilhussaini@users.noreply.github.com>
2022-12-08 17:46:12 +0100

    Add files via upload
    I finished

--- functions.js
+++ functions.js
@@ -1,3 +1,15 @@
-const isTheWordSplit = () => {};
+
+const isTheWordSplit = (pArray) => {
+    let theWord = pArray[0];
+    let array = pArray[1].split(",");
+    for (let index = 0; index < array.length; index++) {
+        for (let k = 0; k < array.length; k++) {
+            if (array[index] + array[k] == theWord) {
+                return array[index] + "," + array[k];
+            }
+        }
+    }
+    return "not possible";
+};
 
 export { isTheWordSplit };
